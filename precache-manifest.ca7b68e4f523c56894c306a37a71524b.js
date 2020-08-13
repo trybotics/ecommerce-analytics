6d1453e068fef8f436f26f90b093bafebcdd7539
unknown <karan.shaw9888@gmail.com>
2020-08-14 04:19:47 +0530

    Updates

---
+++ precache-manifest.ca7b68e4f523c56894c306a37a71524b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822d92d72ed88ca7ac399703789aacce",
+    "url": "/trybotics/ecommerce-analytics/index.html"
+  },
+  {
+    "revision": "d9acd604a38557f5f093",
+    "url": "/trybotics/ecommerce-analytics/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "89de5c3d0021001b38d0",
+    "url": "/trybotics/ecommerce-analytics/static/js/2.9418978f.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/trybotics/ecommerce-analytics/static/js/2.9418978f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9acd604a38557f5f093",
+    "url": "/trybotics/ecommerce-analytics/static/js/main.3779480b.chunk.js"
+  },
+  {
+    "revision": "18f6b3e5fd961cfc88c7",
+    "url": "/trybotics/ecommerce-analytics/static/js/runtime-main.30b0ffe0.js"
+  }
+]);
